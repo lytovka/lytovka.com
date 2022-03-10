fbc66fd0035ffac5b65f0f9ed04a466c08de6257
Ivan Lytovka <ivanlytovka@gmail.com>
2022-03-10 13:57:05 -0500

    add favicon

--- app/root.tsx
+++ app/root.tsx
@@ -32,10 +32,8 @@ export default function App() {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        ></meta>
+        <link rel="icon" type="image/x-icon" href="/public/favicon.ico"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <Meta />
         <Links />
       </head>

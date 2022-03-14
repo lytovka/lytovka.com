9328a8c5cb062708c217fe9934dfaf5494200bcd
Ivan Lytovka <50273967+lytovka@users.noreply.github.com>
2022-03-14 04:17:36 +0200

    feat: add admin page (development environment only)  (#4)
    * update .gitignore
    
    * bind env to window object
    
    * admin page setup
    
    * build edit post page (no stylings for now, just plain old html)
    
    * add return to redirect

---
+++ app/routes/admin.tsx
@@ -0,0 +1,47 @@
+import {
+  Link,
+  LinksFunction,
+  LoaderFunction,
+  Outlet,
+  redirect,
+  useLoaderData,
+} from "remix";
+import { Post } from "~/types/Post";
+import { getPosts } from "~/utils/posts.server";
+import AdminStyles from "~/styles/admin.css";
+
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: AdminStyles,
+    },
+  ];
+};
+
+export const loader: LoaderFunction = async () => {
+  if (process.env.NODE_ENV !== "development") return redirect("/");
+
+  return await getPosts();
+};
+
+export default function Admin() {
+  const posts = useLoaderData<Array<Post>>();
+  return (
+    <div className="admin-container">
+      <nav>
+        <h1>Admin</h1>
+        <ul>
+          {posts.map((post) => (
+            <li key={post.slug}>
+              <Link to={`/admin/edit/${post.slug}`}>{post.title}</Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}

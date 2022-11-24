fcac675e87a5aaed21569bbd5aadbcc2f45a4eef
ivanlytovka <ivanlytovka@gmail.com>
2022-11-24 01:12:35 -0500

    --wip-- [skip ci]

--- app/utils/posts.server.ts
+++ app/utils/posts.server.ts
@@ -1,12 +1,15 @@
 import { marked } from "marked";
 
 import { prisma } from "~/db.server";
+import parseFrontMatter from "front-matter";
 import type { Post } from "~/typings/Post";
+import fs from "fs/promises";
+import path from "path";
 
-export async function getPosts() {
-  const allPosts = await prisma.posts.findMany();
-  return allPosts;
-}
+// export async function getPosts() {
+//   const allPosts = await prisma.posts.findMany();
+//   return allPosts;
+// }
 
 export async function getPost(slug: string): Promise<Post | null> {
   const foundPost = await prisma.posts.findFirst({
@@ -21,4 +24,25 @@ export async function getPost(slug: string): Promise<Post | null> {
   const html = marked(foundPost.markdown);
 
   return { slug, title, html, date: foundPost.date };
+}
+
+export async function getPosts() {
+  const postsPath = await fs.readdir(`${__dirname}/../posts`, {
+    withFileTypes: true,
+  });
+
+  const posts = await Promise.all(
+    postsPath.map(async (dirent) => {
+      const file = await fs.readFile(
+        path.join(`${__dirname}/../posts`, dirent.name)
+      );
+      const { attributes } = parseFrontMatter(file.toString());
+      return {
+        slug: dirent.name.replace(/\.mdx/, ""),
+        //@ts-ignore
+        title: attributes.title,
+      };
+    })
+  );
+  return posts;
 }

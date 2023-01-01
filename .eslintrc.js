e7d5b96cd1b19bf94fb539c99c7e02779020ca13
ivanlytovka <ivanlytovka@gmail.com>
2023-01-01 09:20:37 -0500

    --wip-- [skip ci]

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 /** @type {import('@types/eslint').Linter.BaseConfig} */
 
 module.exports = {
-  extends: ["@lytovka/eslint-config", "@lytovka/eslint-config/react"],
+  extends: [
+    "@lytovka/eslint-config",
+    "@lytovka/eslint-config/react",
+    "@lytovka/eslint-config/jsx-a11y",
+  ],
   parserOptions: {
     tsconfigRootDir: __dirname,
     project: "./tsconfig.json",
   },
   rules: {
     "no-console": "warn",
+    // "jsx-a11y/prefer-tag-over-role": "off",
   },
 };

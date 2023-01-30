2d582b727d5184211494670179c9141176e6cfdd
ivanlytovka <ivanlytovka@gmail.com>
2023-01-29 23:25:15 -0500

    feat: add get albums call from spotify

---
+++ app/routes/collectables.tsx
@@ -0,0 +1,20 @@
+import { useLoaderData } from "@remix-run/react";
+import type { LoaderFunction } from "remix";
+import { json } from "remix";
+
+import { getAlbumsByIds } from "~/server/spotify.server";
+export const loader: LoaderFunction = async () => {
+  return json(
+    await getAlbumsByIds(["3LzKUdUTdJb6P7xGN6SotC", "5Y0p2XCgRRIjna91aQE8q7"])
+  );
+};
+
+export default function CollectablesPage() {
+  const albums = useLoaderData();
+  // console.log(albums);
+  return (
+    <div>
+      <h1>hello</h1>
+    </div>
+  );
+}

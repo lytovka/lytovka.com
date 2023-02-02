26d630c277dabffb85e12f5c1333fdddabffddee
Ivan Lytovka <50273967+lytovka@users.noreply.github.com>
2023-02-02 00:23:26 -0500

    feat: collectibles page (#82)

---
+++ app/routes/collectibles.tsx
@@ -0,0 +1,61 @@
+import { useLoaderData } from "@remix-run/react";
+import { json } from "@remix-run/server-runtime";
+import type { LoaderArgs } from "@remix-run/server-runtime";
+
+import { getAlbumsByIds } from "~/server/spotify.server";
+import { GoBack } from "~/components/go-back";
+import { ExternalLink } from "~/components/external-link";
+
+export const loader = async (_: LoaderArgs) => {
+  return json(
+    await getAlbumsByIds([
+      "3LzKUdUTdJb6P7xGN6SotC",
+      "2u30gztZTylY4RG7IvfXs8",
+      "5Y0p2XCgRRIjna91aQE8q7",
+      "0VwJFPilOR47xaCXnJzB4u",
+      "41KpeN0qV6BBsuJgd8tZrE",
+      "3539EbNgIdEDGBKkUf4wno",
+      "1To7kv722A8SpZF789MZy7",
+      "021D07OEcg0c4tUCilc7ah",
+      "0u3Rl4KquP15smujFrgGz4",
+    ])
+  );
+};
+
+export default function CollectiblesPage() {
+  const { albums } = useLoaderData<typeof loader>();
+  const images = albums.map((a) => ({
+    image: a.images[0],
+    altName: a.name,
+    href: a.external_urls.spotify,
+  }));
+
+  return (
+    <main className="mx-auto px-8 py-10 sm:max-w-5xl md:max-w-7xl">
+      <h1 className="font-medium text-4xl mb-2">Collectibles</h1>
+      <p className="text-2xl text-stone-300 mb-10 italic">
+        Vinyl records I&apos;ve collected over the years.
+      </p>
+
+      <div className="pb-10 grid sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center items-center">
+        {images.map((i, index) => (
+          <ExternalLink
+            href={i.href}
+            key={index}
+            rel="noreferrer noopener"
+            target="_blank"
+          >
+            <img
+              alt={i.altName}
+              className="hover:opacity-75 transition-opacity"
+              height={330}
+              src={i.image.url}
+              width={330}
+            />
+          </ExternalLink>
+        ))}
+      </div>
+      <GoBack />
+    </main>
+  );
+}

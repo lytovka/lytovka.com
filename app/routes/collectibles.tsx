6b892aa3f78758d27c86e2b7e9726a808164eb18
Ivan Lytovka <50273967+lytovka@users.noreply.github.com>
2023-02-03 00:15:31 -0500

    feat: add ping ring to collectibles images on touch devices (#83)

--- app/routes/collectibles.tsx
+++ app/routes/collectibles.tsx
@@ -1,10 +1,13 @@
 import { useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/server-runtime";
 import type { LoaderArgs } from "@remix-run/server-runtime";
+import collectiblesStylesheet from "~/styles/collectibles.css";
 
 import { getAlbumsByIds } from "~/server/spotify.server";
 import { GoBack } from "~/components/go-back";
 import { ExternalLink } from "~/components/external-link";
+import type { LinksFunction } from "@remix-run/server-runtime";
+import { useDeviceType } from "~/hooks/useDeviceType";
 
 export const loader = async (_: LoaderArgs) => {
   return json(
@@ -22,7 +25,17 @@ export const loader = async (_: LoaderArgs) => {
   );
 };
 
+export const links: LinksFunction = () => {
+  return [
+    {
+      rel: "stylesheet",
+      href: collectiblesStylesheet,
+    },
+  ];
+};
+
 export default function CollectiblesPage() {
+  const deviceType = useDeviceType();
   const { albums } = useLoaderData<typeof loader>();
   const images = albums.map((a) => ({
     image: a.images[0],
@@ -40,11 +53,18 @@ export default function CollectiblesPage() {
       <div className="pb-10 grid sm:grid-cols-2 md:grid-cols-3 gap-6 justify-items-center items-center">
         {images.map((i, index) => (
           <ExternalLink
+            className="relative"
             href={i.href}
             key={index}
             rel="noreferrer noopener"
             target="_blank"
           >
+            {deviceType === "mobile" ? (
+              <span className="flex h-5 w-5 absolute right-3 top-3">
+                <span className="ping-class absolute inline-flex h-full w-full rounded-full bg-gray-300" />
+                <span className="absolute inline-flex rounded-full h-full w-full bg-gray-300" />
+              </span>
+            ) : null}
             <img
               alt={i.altName}
               className="hover:opacity-75 transition-opacity"

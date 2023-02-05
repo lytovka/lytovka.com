dff12d75a05a533db6c5803f845d6a4dd070b7a2
ivanlytovka <ivanlytovka@gmail.com>
2023-02-05 01:11:38 -0500

    bring back test

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -1,3 +1,11 @@
+import type { LoaderFunction } from "@remix-run/server-runtime";
+import { json } from "@remix-run/server-runtime";
+import { getPosts } from "~/utils/posts.server";
+
+export const loader: LoaderFunction = async () => {
+    return json(await getPosts());
+  };
+
 export default function NotesRoute() {
   return <h1>index notes route</h1>;
 }

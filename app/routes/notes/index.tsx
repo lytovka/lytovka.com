0f810fa70f658ace6ced18d56f485d945775266e
ivanlytovka <ivanlytovka@gmail.com>
2023-02-06 23:14:24 -0500

    fix: basic table styling

--- app/routes/notes/index.tsx
+++ app/routes/notes/index.tsx
@@ -1,40 +1,39 @@
-import type { LoaderFunction } from "@remix-run/server-runtime";
+import { Link, useLoaderData } from "@remix-run/react";
+import type { LoaderArgs } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
+import { GoBack } from "~/components/go-back";
+import { dateFormatter } from "~/utils/date";
 import { getPosts } from "~/utils/posts.server";
 
-export const loader: LoaderFunction = async () => {
-    return json(await getPosts());
-};
+export const loader = async (_: LoaderArgs) => {
+    const results = await getPosts()
+    const newDates = results.map(item => ({ ...item, date: dateFormatter.format(item.date) }))
+
+    return json(newDates);
+}
 
 export default function NotesRoute() {
+    const posts = useLoaderData<typeof loader>()
+
     return (
         <main className="mx-auto px-8 py-10 sm:max-w-5xl md:max-w-7xl">
-            <table className="table-auto w-full">
+            <table className="table-fixed w-full mb-5">
                 <thead>
                     <tr>
-                        <th className="pb-3 pl-3 text-2xl text-left text-slate-200">Name</th>
-                        <th className="pb-3 pl-3 text-2xl text-left text-slate-200">Size</th>
-                        <th className="pb-3 pl-3 text-2xl text-left text-slate-200">Date Added</th>
+                        <th aria-sort="ascending" className="pr-3 pt-3 pb-3 text-2xl text-left text-slate-200">Name</th>
+                        <th className="pb-3 text-2xl text-left text-slate-200">Last Updated</th>
                     </tr>
                 </thead>
-                <tbody className="">
-                    <tr className="text-2xl">
-                        <td className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, soluta?</td>
-                        <td className="p-3">10</td>
-                        <td className="p-3">10/06/2000</td>
-                    </tr>
-                    <tr className="text-2xl">
-                        <td className="p-3">Lorem ipsum dolor sit amet consectetur adipisicing.</td>
-                        <td className="p-3">7</td>
-                        <td className="p-3">3</td>
-                    </tr>
-                    <tr className="text-2xl">
-                        <td className="p-3">Lorem ipsum dolor sit amet.</td>
-                        <td className="p-3">2</td>
-                        <td className="p-3">3</td>
-                    </tr>
+                <tbody>
+                    {posts.map((post, key) => (
+                        <tr className="text-2xl even:bg-slate-800 pr-3 pt-3 pb-3" key={key}>
+                            <td><Link to={post.slug}><p className="text-slate-300 hover:opacity-75 transition-opacity">{post.title}</p></Link></td>
+                            <td className="pr-3 pt-3 pb-3 text-slate-300">{post.date}</td>
+                        </tr>
+                    ))}
                 </tbody>
             </table>
-        </main>
+            <GoBack />
+        </main >
     )
 }

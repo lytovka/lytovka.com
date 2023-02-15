85e22d48431b111574bf4c473f77cea5a68dc1de
ivanlytovka <ivanlytovka@gmail.com>
2023-02-14 23:51:47 -0500

    add footer and nav

---
+++ app/components/navbar.tsx
@@ -0,0 +1,11 @@
+function Navbar() {
+  return (
+    <div className="z-30 px-5 py-9">
+      <nav className="flex justify-center">
+        <h1 className="text-2xl text-zinc-200">Ivan&apos;s docs</h1>
+      </nav>
+    </div>
+  );
+}
+
+export default Navbar;

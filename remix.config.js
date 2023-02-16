3894aef6ea6986874b8f071af0522efdd311b559
ivanlytovka <ivanlytovka@gmail.com>
2023-02-15 23:21:36 -0500

    try

--- remix.config.js
+++ remix.config.js
@@ -14,5 +14,5 @@ module.exports = {
   serverBuildPath: "api/index.js",
   publicPath: "/build/",
   devServerPort: 8002,
-  serverDependenciesToBundle: ["marked", "front-matter"],
+  serverDependenciesToBundle: ["marked"],
 };

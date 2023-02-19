989b85de6a4fb5775b75a3c007bb626ec8288e96
ivanlytovka <ivanlytovka@gmail.com>
2023-02-19 15:46:49 -0500

    main-layout component

---
+++ app/components/main-layout.tsx
@@ -0,0 +1,21 @@
+import type { PropsWithChildren } from "react";
+import { forwardRef } from "react";
+
+const MainLayout = forwardRef<
+  PropsWithChildren<HTMLElement>,
+  JSX.IntrinsicElements["main"]
+>(function MainLayout(props, ref) {
+  return (
+    <div className="flex-1">
+      <main
+        className="mx-auto px-8 sm:max-w-5xl md:max-w-7xl mb-10"
+        ref={ref}
+        {...props}
+      >
+        {props.children}
+      </main>
+    </div>
+  );
+});
+
+export default MainLayout;

51a9afe72a80edb45ad1e99280879aa8be7d3591
Ivan Lytovka <50273967+lytovka@users.noreply.github.com>
2023-02-18 19:33:35 -0500

    feat: host notes from local `.md` files  (#100)
    Co-authored-by: renovate[bot] <29139614+renovate[bot]@users.noreply.github.com>

--- app/routes/notes/$slug.tsx
+++ app/routes/notes/$slug.tsx
@@ -1,11 +1,11 @@
-import { Link, useCatch, useLoaderData } from "@remix-run/react";
-import formatDate from "date-fns/format";
+import { useCatch, useLoaderData } from "@remix-run/react";
 import { json } from "@remix-run/server-runtime";
-import type { LoaderFunction, MetaFunction } from "@remix-run/server-runtime";
+import type { MetaFunction, LoaderArgs } from "@remix-run/server-runtime";
 
 import type { Post } from "~/typings/Post";
-import { getPost } from "~/utils/posts.server";
 import { FourOhFour, ServerError } from "~/components/errors";
+import { getSlugContent } from "~/server/markdown.server";
+import { dateFormatter } from "~/utils/date";
 
 type LoaderData = Post;
 
@@ -21,51 +21,31 @@ export const meta: MetaFunction = ({ data }: { data: LoaderData | null }) => {
   };
 };
 
-export const loader: LoaderFunction = async ({ params }) => {
+export const loader = async ({ params }: LoaderArgs) => {
   if (!params.slug) {
-    return new Error("params.slug is not defined.");
+    throw new Error("params.slug is not defined.");
   }
+  const res = await getSlugContent(params.slug);
+  res.attributes.date = dateFormatter.format(new Date(res.attributes.date));
 
-  return json(await getPost(params.slug));
+  return json(res);
 };
 
 export default function PostSlug() {
-  const post = useLoaderData<Post>();
-  const postDate = formatDate(new Date(post.date), "dd/MM/yyyy");
+  const note = useLoaderData<typeof loader>();
 
   return (
-    <>
-      <div className="h-24">
-        <nav className="fixed top-0 w-full backdrop-blur border-solid border-b border-gray-400 py-5 px-5">
-          <div className="flex items-center mx-auto max-w-8xl md:px-32">
-            <h1 className="flex-grow font-bold text-3xl text-center ml-6">
-              {post.title}
-            </h1>
-            <Link
-              className="group flex justify-center items-center w-10 h-10 duration-200 hover:bg-red-300 rounded-full"
-              to="/"
-            >
-              <svg
-                className="fill-white duration-200 group-hover:fill-black"
-                fill="currentColor"
-                height={15}
-                viewBox="0 0 371.23 371.23"
-                width={15}
-              >
-                <path d="m371.23 21.213-21.212-21.213-164.403 164.402-164.402-164.402-21.213 21.213 164.402 164.402-164.402 164.403 21.213 21.212 164.402-164.402 164.403 164.402 21.212-21.212-164.402-164.403z" />
-              </svg>
-            </Link>
-          </div>
-        </nav>
-      </div>
-      <article className="py-4 px-8 md:py-8 md:px-32 md:max-w-8xl md:mx-auto">
-        <p className="flex justify-end text-xl mb-3 italic">{postDate}</p>
-        <div
-          className="prose max-w-none text-3xl mb-8"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
-      </article>
-    </>
+    <div className="flex-1">
+      <main className="mx-auto px-8 pb-10 sm:max-w-5xl md:max-w-7xl mb-10 relative">
+        <div>
+          <p className="flex text-xl mb-3 italic">{note.attributes.date}</p>
+          <article
+            className="prose text-3xl"
+            dangerouslySetInnerHTML={{ __html: note.body }}
+          />
+        </div>
+      </main>
+    </div>
   );
 }
 
---
+++ app/server/markdown.server.ts
@@ -0,0 +1,70 @@
+import matter from "gray-matter";
+import fs from "fs/promises";
+import { marked } from "marked";
+import path from "path";
+
+//TODO: This is a hack to get the root path for the app for diff environments (development, CI, and deploy preview). Need to find a better way.
+const root =
+  process.env.NODE_ENV === "development"
+    ? `${__dirname}/../app`
+    : process.env.CI === "true"
+    ? `${path.resolve()}/app`
+    : `${__dirname}/../app`;
+
+type Metadata = {
+  title: string;
+  date: string;
+  slug: string;
+};
+
+// This method is separate from other fetchers because of additional split operation.
+export const getIntroFile = async (): Promise<{
+  short: string;
+  extended: string;
+}> => {
+  const CONTENT = `${root}/markdown`;
+  const pathToIntro = `${CONTENT}/intro.md`;
+  const file = (await fs.readFile(pathToIntro)).toString();
+  const { content } = matter(file);
+  const [short, extended] = marked(content).split("<hr>");
+
+  return { short, extended };
+};
+
+export const getSlugContent = async (
+  slug: string
+): Promise<{
+  attributes: Metadata;
+  body: string;
+}> => {
+  const CONTENT = `${root}/markdown`;
+  const realSlug = slug.replace(/\.md$/, "");
+  const file = (
+    await fs.readFile(`${CONTENT}/notes/${realSlug}.md`)
+  ).toString();
+  const { data, content } = matter(file);
+  const html = marked(content);
+
+  return { attributes: data as Metadata, body: html };
+};
+
+const getAllNoteSlugs = async (): Promise<Array<string>> => {
+  const CONTENT = `${root}/markdown`;
+  const res = fs.readdir(`${CONTENT}/notes`);
+
+  return res;
+};
+
+export const fetchAllContent = async (): Promise<
+  Array<{
+    attributes: { date: string; title: string; slug: string };
+    body: string;
+  }>
+> => {
+  const slugs = await getAllNoteSlugs();
+  const notes = await Promise.all(
+    slugs.map(async (slug) => getSlugContent(slug))
+  );
+
+  return notes;
+};

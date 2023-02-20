5ff7d4e4f41bef7d9b95408828fe354d947325ca
ivanlytovka <ivanlytovka@gmail.com>
2023-02-20 17:07:21 -0500

    feat: add footer trademark

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -3,33 +3,38 @@ import { INSTAGRAM_LINK, TELEGRAM_LINK, GITHUB_LINK } from "~/constants";
 
 function Footer() {
   return (
-    <footer className="py-9 text-xl flex justify-center gap-4 z-30">
-      <ExternalLink
-        className="text-zinc-200 hover:opacity-75 transition-opacity"
-        href={GITHUB_LINK}
-        rel="noreferrer noopener"
-        target="_blank"
-      >
-        GitHub
-      </ExternalLink>
-      <span className="text-white">/</span>
-      <ExternalLink
-        className="text-zinc-200 hover:opacity-75 transition-opacity"
-        href={INSTAGRAM_LINK}
-        rel="noreferrer noopener"
-        target="_blank"
-      >
-        Instagram
-      </ExternalLink>
-      <span className="text-white">/</span>
-      <ExternalLink
-        className="text-zinc-200 hover:opacity-75 transition-opacity"
-        href={TELEGRAM_LINK}
-        rel="noreferrer noopener"
-        target="_blank"
-      >
-        Telegram
-      </ExternalLink>
+    <footer className="flex flex-col items-center py-9 text-xl z-30">
+      <div className="flex gap-4 text-zinc-200">
+        <ExternalLink
+          className="hover:opacity-75 transition-opacity"
+          href={GITHUB_LINK}
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          GitHub
+        </ExternalLink>
+        <span>/</span>
+        <ExternalLink
+          className="hover:opacity-75 transition-opacity"
+          href={INSTAGRAM_LINK}
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          Instagram
+        </ExternalLink>
+        <span>/</span>
+        <ExternalLink
+          className="hover:opacity-75 transition-opacity"
+          href={TELEGRAM_LINK}
+          rel="noreferrer noopener"
+          target="_blank"
+        >
+          Telegram
+        </ExternalLink>
+      </div>
+      <div className="mt-2 text-zinc-400">
+        MIT © Ivan Lytovka 2022-{new Date().getFullYear()}{" "}
+      </div>
     </footer>
   );
 }

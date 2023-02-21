7eccfa9d2512c33a049c6571ae0094abcaab394c
ivanlytovka <ivanlytovka@gmail.com>
2023-02-21 00:40:49 -0500

    remove main styles from the homepage

--- app/components/main-layout.tsx
+++ app/components/main-layout.tsx
@@ -1,16 +1,25 @@
+import clsx from "clsx";
 import type { PropsWithChildren } from "react";
 import { forwardRef } from "react";
 
+type ExtraProps = {
+  homepage?: boolean;
+};
+
 const MainLayout = forwardRef<
   PropsWithChildren<HTMLElement>,
-  JSX.IntrinsicElements["main"]
+  ExtraProps & JSX.IntrinsicElements["main"]
 >(function MainLayout(props, ref) {
+  const { homepage = false, ...rest } = props;
+
   return (
     <div className="flex-1">
       <main
-        className="mx-auto px-8 sm:max-w-5xl md:max-w-7xl mb-10"
+        className={clsx({
+          "mx-auto px-8 sm:max-w-5xl md:max-w-7xl mb-10": !homepage,
+        })}
         ref={ref}
-        {...props}
+        {...rest}
       >
         {props.children}
       </main>

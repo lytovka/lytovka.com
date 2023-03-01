9938e9e9378fb295ad67e6e2978167aa22132db5
ivanlytovka <ivanlytovka@gmail.com>
2023-03-01 00:13:50 -0500

    spotify.server.ts nitpicks

--- app/server/spotify.server.ts
+++ app/server/spotify.server.ts
@@ -4,18 +4,27 @@ type SpotifyAccessToken = {
   expires_in: number;
 };
 
-async function getAccessToken(): Promise<SpotifyAccessToken> {
-  const auth = Buffer.from(
-    `${process.env.SPOTIFY_API_CLIENT_ID}:${process.env.SPOTIFY_API_CLIENT_SECRET}`
-  ).toString("base64");
+const SPOTIFY_ID = process.env.SPOTIFY_API_CLIENT_ID;
+const SPOTIFY_SECRET = process.env.SPOTIFY_API_CLIENT_SECRET;
+const SPOTIFY_API_BASE_URL = process.env.SPOTIFY_API_BASE_URL;
 
-  const data = await fetch(`https://accounts.spotify.com/api/token`, {
+async function getAccessToken(): Promise<SpotifyAccessToken> {
+  const auth = Buffer.from([SPOTIFY_ID, SPOTIFY_SECRET].join(":")).toString(
+    "base64"
+  );
+
+  const tokenUrl = new URL("https://accounts.spotify.com/api/token");
+  const params = new URLSearchParams({
+    grant_type: "client_credentials",
+  });
+
+  const data = await fetch(tokenUrl, {
     method: "POST",
+    body: params,
     headers: {
       "Content-Type": "application/x-www-form-urlencoded",
       Authorization: `Basic ${auth}`,
     },
-    body: "grant_type=client_credentials",
   });
 
   return (await data.json()) as SpotifyAccessToken;
@@ -26,10 +35,13 @@ async function getAlbumsByIds(
 ): Promise<SpotifyApi.MultipleAlbumsResponse> {
   const toRequestParam = ids.join(",");
   const auth = (await getAccessToken()).access_token;
-  const data = await fetch(
-    `${process.env.SPOTIFY_API_BASE_URL}/v1/albums?ids=${toRequestParam}`,
-    { method: "GET", headers: { Authorization: `Bearer ${auth}` } }
-  );
+  const requestUrl = new URL(`${SPOTIFY_API_BASE_URL}/v1/albums`);
+  requestUrl.searchParams.append("ids", toRequestParam);
+
+  const data = await fetch(requestUrl, {
+    method: "GET",
+    headers: { Authorization: `Bearer ${auth}` },
+  });
 
   return data.json() as Promise<SpotifyApi.MultipleAlbumsResponse>;
 }

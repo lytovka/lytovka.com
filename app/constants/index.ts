68a0172b237f69fb903ceb3fbf6929446e499011
ivanlytovka <ivanlytovka@gmail.com>
2023-03-12 22:33:02 -0400

    change key name

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -3,6 +3,6 @@ export const CLOUDINARY_BASE_URL = "https://res.cloudinary.com/lytovka-com";
 export const INSTAGRAM_LINK = "https://www.instagram.com/ivanlytovka/";
 export const TELEGRAM_LINK = "https://t.me/lytovka";
 export const GITHUB_LINK = "https://github.com/lytovka";
-export const LYT_STORAGE_KEY = "lyt";
+export const LYT_STORAGE_KEY = "lyt2";
 export const LINK_HEIGHT_PX = 90;
 export const LINK_WIDTH_PX = 90;

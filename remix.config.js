ff4191c122b2588b10f7af64cef0349cadc4c536
ivanlytovka <ivanlytovka@gmail.com>
2023-05-02 23:06:05 -0400

    feat: upgrade remix to 1.16.0

--- remix.config.js
+++ remix.config.js
@@ -1,13 +1,27 @@
+const { createRoutesFromFolders } = require("@remix-run/v1-route-convention");
+
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+  future: {
+    v2_meta: true,
+  },
+  serverBuildTarget: "vercel",
+  // When running locally in development mode, we use the built in remix
+  // server. This does not understand the vercel lambda module format,
+  // so we default back to the standard build output.
+  // eslint-disable-next-line no-undef
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
+  ignoredRouteFiles: [".*"],
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
   devServerPort: 8002,
-  ignoredRouteFiles: ["**/.*"],
   publicPath: "/build/",
   serverBuildPath: "api/index.js",
   serverDependenciesToBundle: [/^marked/],
   serverMainFields: ["main", "module"],
+  routes(defineRoutes) {
+    return createRoutesFromFolders(defineRoutes);
+  },
 };

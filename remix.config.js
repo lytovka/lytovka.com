9daf4d18cd03782893eecbc21ee4d8074b08162d
ivanlytovka <ivanlytovka@gmail.com>
2023-05-09 21:54:38 -0400

    try something

--- remix.config.js
+++ remix.config.js
@@ -8,18 +8,12 @@ module.exports = {
     v2_meta: true,
     v2_errorBoundary: true,
   },
-  serverBuildTarget: "vercel",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  // eslint-disable-next-line no-undef
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: [".*"],
+  ignoredRouteFiles: ["**/.*"],
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
-  devServerPort: 8002,
+  serverBuildPath: "build/index.js",
   publicPath: "/build/",
-  serverBuildPath: "api/index.js",
+  devServerPort: 8002,
   serverDependenciesToBundle: [/^marked/],
   serverMainFields: ["main", "module"],
   routes(defineRoutes) {

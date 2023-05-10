01ce9b0bcfdf9122f787ca4673e4bf2a35c4c51b
Ivan Lytovka <50273967+lytovka@users.noreply.github.com>
2023-05-09 22:42:13 -0400

    feat: upgrade node to v18 (#156)

--- remix.config.js
+++ remix.config.js
@@ -2,17 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
-  serverBuildTarget: "vercel",
-  // When running locally in development mode, we use the built in remix
-  // server. This does not understand the vercel lambda module format,
-  // so we default back to the standard build output.
-  // eslint-disable-next-line no-undef
-  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
-  ignoredRouteFiles: [".*"],
   appDirectory: "app",
   assetsBuildDirectory: "public/build",
-  serverBuildPath: "api/index.js",
-  publicPath: "/build/",
   devServerPort: 8002,
+  ignoredRouteFiles: ["**/.*"],
+  publicPath: "/build/",
+  serverBuildPath: "api/index.js",
   serverDependenciesToBundle: [/^marked/],
+  serverMainFields: ["main", "module"],
 };

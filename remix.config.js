7473ca70335c0f008d8c5678b059038afa534c0f
ivanlytovka <ivanlytovka@gmail.com>
2023-05-10 21:56:09 -0400

    normalizeformmethod

--- remix.config.js
+++ remix.config.js
@@ -6,6 +6,7 @@ module.exports = {
     v2_routeConvention: true,
     v2_meta: true,
     v2_errorBoundary: true,
+    v2_normalizeFormMethod: true
   },
   appDirectory: "app",
   assetsBuildDirectory: "public/build",

4f6596b04a3de5934df331fb5089bdc13123ebb2
ivanlytovka <ivanlytovka@gmail.com>
2023-05-10 22:15:48 -0400

    feat: add splat route for 404

---
+++ app/routes/$.tsx
@@ -0,0 +1,26 @@
+import { Links, Meta, Scripts } from "@remix-run/react";
+import type { LoaderFunction } from "@remix-run/server-runtime";
+import { FourOhFour } from "~/components/errors";
+
+export function loader(): LoaderFunction {
+  throw new Response("Not Found", { status: 404 });
+}
+
+export default function _() {
+  return (
+    <html lang="en">
+      <head>
+        <title>My Nested Error</title>
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Scripts />
+      </body>
+    </html>
+  );
+}
+
+export function ErrorBoundary() {
+  return <FourOhFour />;
+}

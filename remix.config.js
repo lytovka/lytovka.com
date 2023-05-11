0cde0f3b07c73f551b51c98e00fd8a99ff5fd50c
ivanlytovka <ivanlytovka@gmail.com>
2023-05-10 22:15:26 -0400

    fix: turn off no-unsafe-return

--- remix.config.js
+++ remix.config.js
@@ -6,7 +6,7 @@ module.exports = {
     v2_routeConvention: true,
     v2_meta: true,
     v2_errorBoundary: true,
-    v2_normalizeFormMethod: true
+    v2_normalizeFormMethod: true,
   },
   appDirectory: "app",
   assetsBuildDirectory: "public/build",

61831b95885d9c0905ca29caf68585f12466079e
ivanlytovka <ivanlytovka@gmail.com>
2023-07-17 10:35:03 -0400

    try something

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,25 +1,23 @@
-import { hydrate } from "react-dom";
 import { RemixBrowser } from "@remix-run/react";
+import { startTransition, StrictMode } from "react";
+import { hydrateRoot } from "react-dom/client";
+import i18n from "~/i18nextOptions";
 import i18next from "i18next";
 import { I18nextProvider, initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import Backend from "i18next-http-backend";
 import { getInitialNamespaces } from "remix-i18next";
-import i18nextOptions from "./i18nextOptions";
 
-// initialize i18next using initReactI18next and configuring it
-if (!i18next.isInitialized) {
-  // prevent i18next to be initialized multiple times
-  void i18next
+async function hydrate() {
+  await i18next
     .use(initReactI18next) // Tell i18next to use the react-i18next plugin
     .use(LanguageDetector) // Setup a client-side language detector
     .use(Backend) // Setup your backend
     .init({
-      ...i18nextOptions,
-      backend: { loadPath: "/locales/{{lng}}/{{ns}}.json" },
+      ...i18n, // spread the configuration
       // This function detects the namespaces your routes rendered while SSR use
-      // and pass them here to load the translations
       ns: getInitialNamespaces(),
+      backend: { loadPath: "/locales/{{lng}}/{{ns}}.json" },
       detection: {
         // Here only enable htmlTag detection, we'll detect the language only
         // server-side with remix-i18next, by using the `<html lang>` attribute
@@ -29,14 +27,25 @@ if (!i18next.isInitialized) {
         // on the browser, so we disable it
         caches: [],
       },
-    })
-    .then(() => {
-      // then hydrate your app wrapped in the I18nextProvider
-      hydrate(
-        <I18nextProvider i18n={i18next}>
-          <RemixBrowser />
-        </I18nextProvider>,
-        document,
-      );
     });
+
+  startTransition(() => {
+    hydrateRoot(
+      document,
+      <I18nextProvider i18n={i18next}>
+        <StrictMode>
+          <RemixBrowser />
+        </StrictMode>
+      </I18nextProvider>
+    );
+  });
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+if (window.requestIdleCallback) {
+  window.requestIdleCallback(hydrate);
+} else {
+  // Safari doesn't support requestIdleCallback
+  // https://caniuse.com/requestidlecallback
+  window.setTimeout(hydrate, 1);
 }
--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,56 +1,76 @@
-import { RemixServer } from "@remix-run/react";
+import { PassThrough } from "stream";
 import type { EntryContext } from "@remix-run/node";
-import { config } from "dotenv";
-import { renderToString } from "react-dom/server";
+import { Response } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
+import { renderToPipeableStream } from "react-dom/server";
 import { createInstance } from "i18next";
-import Backend from "i18next-fs-backend";
-import i18n from "./server/i18n.server";
+import i18next from "./server/i18n.server";
 import { I18nextProvider, initReactI18next } from "react-i18next";
-import i18nextOptions from "~/i18nextOptions";
-import { resolve } from "path";
+import Backend from "i18next-fs-backend";
+import i18n from "~/i18nextOptions"; // your i18n configuration file
+import { resolve } from "node:path";
+import { config } from "dotenv";
 
 config();
+
+const ABORT_DELAY = 5000;
 
 export default async function handleRequest(
   request: Request,
   responseStatusCode: number,
   responseHeaders: Headers,
-  remixContext: EntryContext,
+  remixContext: EntryContext
 ) {
-  // First, we create a new instance of i18next so every request will have a
-  // completely unique instance and not share any state
-  const instance = createInstance();
+  const callbackName = "onShellReady";
 
-  // Then we could detect locale from the request
-  const lng = await i18n.getLocale(request);
-  // And here we detect what namespaces the routes about to render want to use
-  const ns = i18n.getRouteNamespaces(remixContext);
-  // First, we create a new instance of i18next so every request will have a
-  // completely unique instance and not share any state.
+  const instance = createInstance();
+  const lng = await i18next.getLocale(request);
+  const ns = i18next.getRouteNamespaces(remixContext);
+
   await instance
     .use(initReactI18next) // Tell our instance to use react-i18next
-    .use(Backend) // Setup our backend.init({
+    .use(Backend) // Setup our backend
     .init({
-      ...i18nextOptions, // use the same configuration as in your client side.
+      ...i18n, // spread the configuration
       lng, // The locale we detected above
-      ns, // The namespaces the routes about to render want to use
-      backend: {
-        loadPath: resolve("./public/locales/{{lng}}/{{ns}}.json"),
-      },
+      ns, // The namespaces the routes about to render wants to use
+      backend: { loadPath: resolve("./public/locales/{{lng}}/{{ns}}.json") },
     });
 
-  // Then you can render your app wrapped in the I18nextProvider as in the
-  // entry.client file
-  const markup = renderToString(
-    <I18nextProvider i18n={instance}>
-      <RemixServer context={remixContext} url={request.url} />
-    </I18nextProvider>,
-  );
+  // eslint-disable-next-line @typescript-eslint/no-shadow
+  return new Promise((resolve, reject) => {
+    let didError = false;
 
-  responseHeaders.set("Content-Type", "text/html");
+    const { pipe, abort } = renderToPipeableStream(
+      <I18nextProvider i18n={instance}>
+        <RemixServer context={remixContext} url={request.url} />
+      </I18nextProvider>,
+      {
+        [callbackName]: () => {
+          const body = new PassThrough();
 
-  return new Response(`<!DOCTYPE html>${markup}`, {
-    status: responseStatusCode,
-    headers: responseHeaders,
+          responseHeaders.set("Content-Type", "text/html");
+
+          resolve(
+            new Response(body, {
+              headers: responseHeaders,
+              status: didError ? 500 : responseStatusCode,
+            })
+          );
+
+          pipe(body);
+        },
+        onShellError(error: unknown) {
+          reject(error);
+        },
+        onError(error: unknown) {
+          didError = true;
+
+          console.error(error);
+        },
+      }
+    );
+
+    setTimeout(abort, ABORT_DELAY);
   });
 }

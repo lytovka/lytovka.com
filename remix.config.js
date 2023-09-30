fccd5454cf155e00e51e47384deb21e95b5679eb
ivanlytovka <ivanlytovka@gmail.com>
2023-09-29 23:14:21 -0400

    try something

--- remix.config.js
+++ remix.config.js
@@ -2,13 +2,13 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 export default {
-  tailwind: true,
-  postcss: true,
+  // tailwind: true,
+  // postcss: true,
   ignoredRouteFiles: ["**/.*"],
-  appDirectory: "app",
-  assetsBuildDirectory: "public/build",
-  serverBuildPath: "build/index.js",
-  publicPath: "/build/",
+  // appDirectory: "app",
+  // assetsBuildDirectory: "public/build",
+  // serverBuildPath: "build/index.js",
+  // publicPath: "/build/",
   serverDependenciesToBundle: [/^marked/],
-  serverModuleFormat: "esm",
+  // serverModuleFormat: "esm",
 };

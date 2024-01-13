fa782b73ccc4584d291fe65d40aaf4bef09d5449
ivanlytovka <ivanlytovka@gmail.com>
2024-01-13 15:45:44 -0500

    feat: migrate to vite compiler

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { unstable_vitePlugin as remix } from "@remix-run/dev";
+import { defineConfig } from "vite";
+import { installGlobals } from "@remix-run/node";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+installGlobals();
+
+export default defineConfig({
+  plugins: [
+    remix({
+      ignoredRouteFiles: ["**/.*"],
+    }),
+    tsconfigPaths(),
+  ],
+  ssr: {
+    noExternal: [/^marked/],
+  },
+});

b2b655128fb38863fb497b6ce05a4b701d23bfeb
ivanlytovka <ivanlytovka@gmail.com>
2024-03-03 07:12:25 -0500

    fix root path

--- app/server/markdown.server.ts
+++ app/server/markdown.server.ts
@@ -3,12 +3,8 @@ import matter from "gray-matter";
 import fs from "fs/promises";
 import { marked } from "marked";
 import previews from "~/markdown/notes/previews.json";
-import { fileURLToPath } from "url";
 
-//TODO: This is a hack to get the root path for the app for diff environments (test, CI, and deploy preview). Need to find a better way.
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const root =
-  process.env.CI === "true" ? `${path.resolve()}/app` : `${__dirname}/../`;
+const root = `${path.resolve()}/app`;
 
 type Metadata = {
   title: string;

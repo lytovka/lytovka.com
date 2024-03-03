0576905e97f03097fd6d2b0b8880607edbab27de
ivanlytovka <ivanlytovka@gmail.com>
2024-03-03 06:59:42 -0500

    load md file as module

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { vitePlugin as remix } from "@remix-run/dev";
-=======
-import { unstable_vitePlugin as remix } from "@remix-run/dev";
->>>>>>> fa782b7 (feat: migrate to vite compiler)
-=======
-import { vitePlugin as remix } from "@remix-run/dev";
->>>>>>> a5a020f (update remix)
 import { defineConfig } from "vite";
 import { installGlobals } from "@remix-run/node";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -18,7 +10,6 @@ export default defineConfig({
     remix({
       ignoredRouteFiles: ["**/.*"],
     }),
-<<<<<<< HEAD
     // Custom plugin to load markdown files
     {
       name: "markdown-loader",
@@ -29,8 +20,6 @@ export default defineConfig({
         }
       },
     },
-=======
->>>>>>> fa782b7 (feat: migrate to vite compiler)
     tsconfigPaths(),
   ],
   ssr: {

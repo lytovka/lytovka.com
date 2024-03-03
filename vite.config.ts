70090c9d7f7f4e151d9a7d23cdb3b143dc47690d
ivanlytovka <ivanlytovka@gmail.com>
2024-03-03 06:16:10 -0500

    update remix

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { vitePlugin as remix } from "@remix-run/dev";
 =======
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
 >>>>>>> fa782b7 (feat: migrate to vite compiler)
+=======
+import { vitePlugin as remix } from "@remix-run/dev";
+>>>>>>> a5a020f (update remix)
 import { defineConfig } from "vite";
 import { installGlobals } from "@remix-run/node";
 import tsconfigPaths from "vite-tsconfig-paths";

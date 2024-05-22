90992c698d2328bad884740c8fdf28a6a0c60aa7
ivanlytovka <ivanlytovka@gmail.com>
2024-05-21 21:12:58 -0400

    revert entry client change

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,12 +1,4 @@
 import { RemixBrowser } from "@remix-run/react";
-import { StrictMode, startTransition } from "react";
 import { hydrateRoot } from "react-dom/client";
 
-startTransition(() => {
-  hydrateRoot(
-    document,
-    <StrictMode>
-      <RemixBrowser />
-    </StrictMode>,
-  );
-});
+hydrateRoot(document, <RemixBrowser />);

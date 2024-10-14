8503b44a85164772c3b7cdb020fd833d6999e6b6
ivanlytovka <ivanlytovka@gmail.com>
2024-10-13 23:01:34 -0400

    remove web component

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -1,11 +1,9 @@
 import { RemixBrowser } from "@remix-run/react";
 import { startTransition, StrictMode } from "react";
 import { hydrateRoot } from "react-dom/client";
-import HelloWorld from "~/components/hello-world";
 
 // initialize all web components
 startTransition(() => {
-  customElements.define("image-carousel", HelloWorld);
   hydrateRoot(
     document,
     <StrictMode>

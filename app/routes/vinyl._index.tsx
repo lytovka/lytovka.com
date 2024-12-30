cd906ef8c0f12679ca1c78fb28147f7c999486b7
ivanlytovka <ivanlytovka@gmail.com>
2024-12-30 01:20:27 -0500

    log route error

--- app/routes/vinyl._index.tsx
+++ app/routes/vinyl._index.tsx
@@ -1,202 +1,206 @@
-import { Await, useLoaderData } from "@remix-run/react";
+import {Await, useLoaderData, useRouteError} from "@remix-run/react";
 import "~/styles/vinyl.css";
 
-import { defer } from "@remix-run/node";
+import {defer} from "@remix-run/node";
 import {
-  SPOTIFY_COUNT_LIMIT,
-  getAlbumsByIds,
+    SPOTIFY_COUNT_LIMIT,
+    getAlbumsByIds,
 } from "~/server/spotify.server.ts";
 import GoBack from "~/components/go-back.tsx";
-import { ExternalLink } from "~/components/external-link.tsx";
-import { ServerError } from "~/components/errors.tsx";
-import { H1, Paragraph } from "~/components/typography.tsx";
+import {ExternalLink} from "~/components/external-link.tsx";
+import {ServerError} from "~/components/errors.tsx";
+import {H1, Paragraph} from "~/components/typography.tsx";
 import MainLayout from "~/components/main-layout.tsx";
 import {
-  getMetadataUrl,
-  getPreviewUrl,
-  getSocialImagePreview,
-  getSocialMetas,
+    getMetadataUrl,
+    getPreviewUrl,
+    getSocialImagePreview,
+    getSocialMetas,
 } from "~/utils/seo.ts";
-import type { MetaFunction } from "@vercel/remix";
-import type { RootLoaderDataUnwrapped } from "~/root.tsx";
-import { prisma } from "~/server/db";
-import { Suspense, useEffect, useRef } from "react";
-import { splitIntoChunks } from "~/utils/array";
-import { Skeleton } from "~/components/ui/skeleton";
+import type {MetaFunction} from "@vercel/remix";
+import type {RootLoaderDataUnwrapped} from "~/root.tsx";
+import {prisma} from "~/server/db";
+import {Suspense, useEffect, useRef} from "react";
+import {splitIntoChunks} from "~/utils/array";
+import {Skeleton} from "~/components/ui/skeleton";
 
-export const meta: MetaFunction<typeof loader> = ({ matches }) => {
-  const { requestInfo } = (matches[0] as RootLoaderDataUnwrapped).data;
-  const metadataUrl = getMetadataUrl(requestInfo);
+export const meta: MetaFunction<typeof loader> = ({matches}) => {
+    const {requestInfo} = (matches[0] as RootLoaderDataUnwrapped).data;
+    const metadataUrl = getMetadataUrl(requestInfo);
 
-  return [
-    {
-      name: "viewport",
-      content: "width=device-width,initial-scale=1,viewport-fit=cover",
-    },
-    ...getSocialMetas({
-      title: "Ivan's vinyl collection",
-      keywords: "vinyl, collectibles, ivan lytovka, lytovka",
-      url: metadataUrl,
-      description: "A collection of vinyl records Ivan owns.",
-      image: getSocialImagePreview({
-        title: "vinyl",
-        url: getPreviewUrl(metadataUrl),
-        featuredImage: "vinyl",
-      }),
-    }),
-  ];
+    return [
+        {
+            name: "viewport",
+            content: "width=device-width,initial-scale=1,viewport-fit=cover",
+        },
+        ...getSocialMetas({
+            title: "Ivan's vinyl collection",
+            keywords: "vinyl, collectibles, ivan lytovka, lytovka",
+            url: metadataUrl,
+            description: "A collection of vinyl records Ivan owns.",
+            image: getSocialImagePreview({
+                title: "vinyl",
+                url: getPreviewUrl(metadataUrl),
+                featuredImage: "vinyl",
+            }),
+        }),
+    ];
 };
 
 export function loader() {
-  const albums = prisma.album
-    .findMany({
-      select: { spotifyId: true, description: true },
-    })
-    .then((albumsDb) =>
-      splitIntoChunks(albumsDb, SPOTIFY_COUNT_LIMIT).map((albumChunk) =>
-        getAlbumsByIds(albumChunk.map((a) => a.spotifyId)),
-      ),
-    )
-    .then((all) => Promise.all(all))
-    .then((arrays) => arrays.flat())
-    .catch((error) => {
-      throw new Error(`Could not load albums from Spotify: ${error}`);
-    });
+    const albums = prisma.album
+        .findMany({
+            select: {spotifyId: true, description: true},
+        })
+        .then((albumsDb) =>
+            splitIntoChunks(albumsDb, SPOTIFY_COUNT_LIMIT).map((albumChunk) =>
+                getAlbumsByIds(albumChunk.map((a) => a.spotifyId)),
+            ),
+        )
+        .then((all) => Promise.all(all))
+        .then((arrays) => arrays.flat())
+        .catch((error) => {
+            throw new Error(`Could not load albums from Spotify: ${error}`);
+        });
 
-  return defer({ albumRows: albums } as const, {
-    headers: { "Cache-Control": "public, max-age=96400" },
-  });
+    return defer({albumRows: albums} as const, {
+        headers: {"Cache-Control": "public, max-age=96400"},
+    });
 }
 
 function VinylSkeleton() {
-  return (
-    <div className="w-full mb-12">
-      {[1, 2].map((_, index) => (
-        <div
-          className="scroll-container overflow-scroll mb-5 flex flex-row grow gap-2 relative"
-          key={index}
-        >
-          {[1, 2, 3].map((album, i) => (
-            <div className="shrink-0 flex items-center px-2" key={i}>
-              <div className="inline-flex">
-                <figure className="flex flex-col gap-1">
-                  <Skeleton className="w-[290px] h-[290px]" />
-                  <figcaption className="flex flex-col gap-1 items-center justify-center">
-                    <Skeleton className="w-[100px] h-4" />
-                    <Skeleton className="w-[100px] h-4" />
-                  </figcaption>
-                </figure>
-              </div>
-            </div>
-          ))}
+    return (
+        <div className="w-full mb-12">
+            {[1, 2].map((_, index) => (
+                <div
+                    className="scroll-container overflow-scroll mb-5 flex flex-row grow gap-2 relative"
+                    key={index}
+                >
+                    {[1, 2, 3].map((album, i) => (
+                        <div className="shrink-0 flex items-center px-2" key={i}>
+                            <div className="inline-flex">
+                                <figure className="flex flex-col gap-1">
+                                    <Skeleton className="w-[290px] h-[290px]"/>
+                                    <figcaption className="flex flex-col gap-1 items-center justify-center">
+                                        <Skeleton className="w-[100px] h-4"/>
+                                        <Skeleton className="w-[100px] h-4"/>
+                                    </figcaption>
+                                </figure>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 }
 
 export default function VinylPage() {
-  const dataStream = useLoaderData<typeof loader>();
-  const containerRefs = useRef<Array<HTMLDivElement | null>>([]);
+    const dataStream = useLoaderData<typeof loader>();
+    const containerRefs = useRef<Array<HTMLDivElement | null>>([]);
 
-  useEffect(() => {
-    const step = 1;
-    const delay = 20;
-    let lastFrameTime = performance.now();
+    useEffect(() => {
+        const step = 1;
+        const delay = 20;
+        let lastFrameTime = performance.now();
 
-    const autoScroll = (currentTime: number) => {
-      if (currentTime - lastFrameTime >= delay) {
-        containerRefs.current.forEach((ref, index) => {
-          if (!ref) {
+        const autoScroll = (currentTime: number) => {
+            if (currentTime - lastFrameTime >= delay) {
+                containerRefs.current.forEach((ref, index) => {
+                    if (!ref) {
+                        requestAnimationFrame(autoScroll);
+
+                        return;
+                    }
+                    if (index % 2 === 0) {
+                        ref.scrollLeft = Math.ceil(ref.scrollLeft + step);
+                        if (ref.scrollLeft >= ref.scrollWidth / 2) {
+                            ref.scrollLeft = 0;
+                        }
+                    }
+                    if (index % 2 === 1) {
+                        ref.scrollLeft = Math.ceil(ref.scrollLeft - step);
+                        if (ref.scrollLeft <= 0) {
+                            ref.scrollLeft = ref.scrollWidth / 2;
+                        }
+                    }
+                });
+                lastFrameTime = currentTime;
+            }
+
             requestAnimationFrame(autoScroll);
+        };
 
-            return;
-          }
-          if (index % 2 === 0) {
-            ref.scrollLeft = Math.ceil(ref.scrollLeft + step);
-            if (ref.scrollLeft >= ref.scrollWidth / 2) {
-              ref.scrollLeft = 0;
-            }
-          }
-          if (index % 2 === 1) {
-            ref.scrollLeft = Math.ceil(ref.scrollLeft - step);
-            if (ref.scrollLeft <= 0) {
-              ref.scrollLeft = ref.scrollWidth / 2;
-            }
-          }
-        });
-        lastFrameTime = currentTime;
-      }
+        autoScroll(lastFrameTime);
+    }, []);
 
-      requestAnimationFrame(autoScroll);
-    };
+    return (
+        <MainLayout className="px-0 md:px-8">
+            <div className="flex flex-col gap-1 mb-10 px-8 md:px-0">
+                <H1 className="font-bold">Vinyl</H1>
+                <Paragraph className="italic" variant="secondary">
+                    A small collection of vinyl records I own. Images are clickable.
+                </Paragraph>
+            </div>
 
-    autoScroll(lastFrameTime);
-  }, []);
+            <Suspense fallback={<VinylSkeleton/>}>
+                <Await resolve={dataStream.albumRows}>
+                    {(data) => {
+                        const albumChunks = splitIntoChunks(data, 5);
 
-  return (
-    <MainLayout className="px-0 md:px-8">
-      <div className="flex flex-col gap-1 mb-10 px-8 md:px-0">
-        <H1 className="font-bold">Vinyl</H1>
-        <Paragraph className="italic" variant="secondary">
-          A small collection of vinyl records I own. Images are clickable.
-        </Paragraph>
-      </div>
+                        return (
+                            <div className="w-full mb-12">
+                                {albumChunks.map((albumRow, index) => (
+                                    <div
+                                        className="scroll-container mb-5 flex flex-row grow overflow-x-scroll relative"
+                                        key={index}
+                                        // @ts-ignore TODO
+                                        ref={(ref) => {
+                                            containerRefs.current[index] = ref;
 
-      <Suspense fallback={<VinylSkeleton />}>
-        <Await resolve={dataStream.albumRows}>
-          {(data) => {
-            const albumChunks = splitIntoChunks(data, 5);
-
-            return (
-              <div className="w-full mb-12">
-                {albumChunks.map((albumRow, index) => (
-                  <div
-                    className="scroll-container mb-5 flex flex-row grow overflow-x-scroll relative"
-                    key={index}
-                    // @ts-ignore TODO
-                    ref={(ref) => {
-                      containerRefs.current[index] = ref;
-
-                      return ref;
+                                            return ref;
+                                        }}
+                                    >
+                                        {[...albumRow, ...albumRow].map((album, i) => (
+                                            <div
+                                                className="shrink-0 flex items-center w-[300px] px-2"
+                                                key={i}
+                                            >
+                                                <ExternalLink
+                                                    href={album.href}
+                                                    rel="noreferrer noopener"
+                                                    target="_blank"
+                                                >
+                                                    <figure className="flex flex-col gap-1">
+                                                        <img
+                                                            alt={album.altName}
+                                                            className="w-[290px] h-[290px] border border-gray-300 dark:border-gray-700 hover:opacity-75 transition-opacity"
+                                                            src={album.image.url}
+                                                        />
+                                                        <figcaption
+                                                            className="flex flex-col gap-1 items-center justify-center">
+                                                            <span>{album.name}</span>
+                                                            <span className="text-sm">{album.artists}</span>
+                                                        </figcaption>
+                                                    </figure>
+                                                </ExternalLink>
+                                            </div>
+                                        ))}
+                                    </div>
+                                ))}
+                            </div>
+                        );
                     }}
-                  >
-                    {[...albumRow, ...albumRow].map((album, i) => (
-                      <div
-                        className="shrink-0 flex items-center w-[300px] px-2"
-                        key={i}
-                      >
-                        <ExternalLink
-                          href={album.href}
-                          rel="noreferrer noopener"
-                          target="_blank"
-                        >
-                          <figure className="flex flex-col gap-1">
-                            <img
-                              alt={album.altName}
-                              className="w-[290px] h-[290px] border border-gray-300 dark:border-gray-700 hover:opacity-75 transition-opacity"
-                              src={album.image.url}
-                            />
-                            <figcaption className="flex flex-col gap-1 items-center justify-center">
-                              <span>{album.name}</span>
-                              <span className="text-sm">{album.artists}</span>
-                            </figcaption>
-                          </figure>
-                        </ExternalLink>
-                      </div>
-                    ))}
-                  </div>
-                ))}
-              </div>
-            );
-          }}
-        </Await>
-      </Suspense>
-      <GoBack className="px-8 md:px-0" />
-    </MainLayout>
-  );
+                </Await>
+            </Suspense>
+            <GoBack className="px-8 md:px-0"/>
+        </MainLayout>
+    );
 }
 
 export function ErrorBoundary() {
-  return <ServerError title="Could not load albums from Spotify." />;
+    const error = useRouteError();
+    console.log("ErrorBoundary", error)
+
+    return <ServerError title="Could not load albums from Spotify."/>;
 }

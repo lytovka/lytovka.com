470b8fd864f5a727487bfd6272eac5a6a5fa49c7
Ivan Lytovka <ivanlytovka@gmail.com>
2025-11-30 13:47:43 -0500

    page header

--- app/components/page-header.tsx
+++ app/components/page-header.tsx
@@ -11,11 +11,11 @@ export function PageHeader({ title, subtitle, className }: PageHeaderProps) {
   return (
     <div className={className}>
       <H1 className="font-bold text-3xl md:text-4xl">{title}</H1>
-      {subtitle && (
+      {subtitle ? (
         <p className="text-stone-600 dark:text-stone-400 text-base md:text-lg mt-1">
           {subtitle}
         </p>
-      )}
+      ) : null}
     </div>
   );
 }
